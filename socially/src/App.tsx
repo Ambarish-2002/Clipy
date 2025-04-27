d1565b1480781d49b971823c267b539fe957f4b4
Ambarish-2002 <amb.kshirsagar@gmail.com>
2025-04-26 19:50:11 -0700

    Added Navbar

--- socially/src/App.tsx
+++ socially/src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
     <div>
+      <Navbar/>
       <h1>
         MY FIRST CHROME EXTENSION
       </h1>
---
+++ socially/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='navbar__logo'>
+            <div className='image-container'>
+                <img src = 'logo.png' alt = 'logo'  width ="50px" height = "50px"/>    
+            </div>
+            <h1>SOCIALY</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
