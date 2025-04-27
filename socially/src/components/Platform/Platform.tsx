f5ecaeb8776eae63819b8be84653b0262562e21f
Ambarish-2002 <amb.kshirsagar@gmail.com>
2025-04-26 20:39:30 -0700

    added platform component

--- socially/src/components/Platform/Platform.tsx
+++ socially/src/components/Platform/Platform.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
+import { fetchYtTrends } from '../../services/fetchTrends';
+import './platform.css'
 const Platform = () => {
+
+    const getYtTrends  = async () => {
+        const data = await fetchYtTrends();
+        console.log(data);
+    }
+
   return (
-    <div>
-      
+    <div className='platform-choose'>
+        <button onClick={() => getYtTrends()}>
+            <img src="logo.png" alt="" width="50px" height ="50px"/>
+        </button>
+
+        <button onClick={() => getYtTrends()}>
+            <img src="logo.png" alt="" width="50px" height ="50px"/>
+        </button>
+
+        <button onClick={() => getYtTrends()}>
+            <img src="logo.png" alt="" width="50px" height ="50px"/>
+        </button>
+
     </div>
   )
 }
