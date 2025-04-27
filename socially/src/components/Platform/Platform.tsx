61d89f48a15b2dad95c27ad5da3a494757b1d322
Ambarish-2002 <amb.kshirsagar@gmail.com>
2025-04-27 16:11:49 -0700

    added icons

--- socially/src/components/Platform/Platform.tsx
+++ socially/src/components/Platform/Platform.tsx
@@ -9,16 +9,16 @@ const Platform = () => {
 
   return (
     <div className='platform-choose'>
-        <button onClick={() => getYtTrends()}>
-            <img src="logo.png" alt="" width="50px" height ="50px"/>
+        <button className='platform-button-yt' onClick={() => getYtTrends()}>
+            <img src="youtube_icon.svg" alt="" width="50px" height ="50px"/>
         </button>
 
-        <button onClick={() => getYtTrends()}>
-            <img src="logo.png" alt="" width="50px" height ="50px"/>
+        <button className='platform-button-ig' onClick={() => getYtTrends()}>
+            <img src="insta_icon.svg" alt="" width="50px" height ="50px"/>
         </button>
 
-        <button onClick={() => getYtTrends()}>
-            <img src="logo.png" alt="" width="50px" height ="50px"/>
+        <button className='platform-button-tw' onClick={() => getYtTrends()}>
+            <img src="twitter_icon.svg" alt="" width="50px" height ="50px"/>
         </button>
 
     </div>
